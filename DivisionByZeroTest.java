06d57129a0fd1a11d535a5fd9995a6e73295c175
IlyaStrnnkv <91563364+IlyaStrnnkv@users.noreply.github.com>
2022-02-02 01:52:11 +0400

    Create DivisionByZeroTest.java

---
+++ DivisionByZeroTest.java
@@ -0,0 +1,16 @@
+package tests.negative.division;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.base.BaseTest;
+
+public class DivisionByZeroTest extends BaseTest {
+    @Test(priority = 2)
+    public void divisionByZero () {
+        googleHomePage.open("https://www.google.com");
+        googleHomePage.enterText().clickToFind();
+        calculatorPage.sixDivideByZero().getAnswer();
+        Assert.assertEquals(calculatorPage.answerValueStringForDiv(), "Infinity");
+    }
+
+}
